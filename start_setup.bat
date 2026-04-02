1242130706426df406368f5f87cd21d7524f25a0
AmareshSM <87269971+AmareshMuddebihal@users.noreply.github.com>
2026-04-02 22:26:32 +0530

    Update start_setup.bat

--- start_setup.bat
+++ start_setup.bat
@@ -2,7 +2,7 @@
 cd /d "%~dp0"
 echo Running Laptop Setup...
 
-powershell -NoProfile -ExecutionPolicy Bypass -File "setup_phase1.ps1"
+powershell -NoProfile -ExecutionPolicy Bypass -File "1setup_phase1.ps1"
 
 echo.
 echo If nothing happened, check errors above.
